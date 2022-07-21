529d03293fb49912303d1b3c2356aba2ebcdf4f3
000VIJAY <vijay893689@gmail.com>
2022-07-21 20:50:06 +0530

    [Vijay] Added program for FindList of rides or each user

---
+++ CabInvoiceProblem/InvoiceForeachUser.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CabInvoiceProblem;
+
+namespace CabInvoiceProblem
+{
+    public class InvoiceForeachUser
+    {
+        Dictionary<int, List<Ride>> repo = new Dictionary<int, List<Ride>>();
+
+        public EnhencedInvoice UserFare(int userId)
+        {
+            Invoice cabInvoice = new Invoice();
+            List<Ride> invoices = new List<Ride>();
+
+            EnhencedInvoice res = new EnhencedInvoice(0, 0);
+            if (repo.ContainsKey(userId))
+            {
+                invoices = repo[userId];
+
+                if (invoices != null && invoices.Count > 0)
+                {
+                    res = cabInvoice.MultipleRideFair(repo[userId]);
+                }
+            }
+            return res;
+        }
+
+        public void AddRides(int userId, List<Ride> rides)
+        {
+            List<Ride> invoices = new List<Ride>();
+            if (repo.ContainsKey(userId))
+            {
+                invoices = repo[userId];
+            }
+
+            foreach (var ride in rides)
+            {
+                invoices.Add(ride);
+            }
+            repo[userId] = invoices;
+        }
+    }
+}
---
+++ CabInvoiceTest/UserInvoicePerUser.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CabInvoiceProblem;
+
+namespace CabInvoiceTest
+{
+    [TestClass]
+    public class UserInvoice
+    {
+        [TestMethod]
+        public void TestInvoicePerUser()
+        {
+            int userId = 1;
+            InvoiceForeachUser IPU = new InvoiceForeachUser();
+            IPU.AddRides(1, new List<Ride>()
+            {
+                new Ride(20, 30),   //230
+                new Ride(2, 3),     //23
+                new Ride(12, 16),   //136
+                new Ride(5, 7),     //57
+                new Ride(19, 23)    //213
+            });
+            IPU.AddRides(2, new List<Ride>()
+            {
+                new Ride(20, 30),   //230
+                new Ride(2, 3),     //23
+                new Ride(12, 16),   //136
+                new Ride(5, 7),     //57
+                new Ride(19, 23)    //213
+            });
+
+            var res = IPU.UserFare(userId);
+            Assert.AreEqual(659, res.totalFair);
+        }
+
+    }
+}
